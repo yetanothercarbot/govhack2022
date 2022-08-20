dc84eb034ca2c08d5ccf9b96553e114c5255bea4
yetanothercarbot <55005345+yetanothercarbot@users.noreply.github.com>
2022-08-20 14:55:38 +1000

    Add data source for heavy vehicle routes. Display this on the map

--- webpage/js/main.js
+++ webpage/js/main.js
@@ -1,33 +1,141 @@
 import '../css/style.css';
 import {Map, View} from 'ol';
-import TileLayer from 'ol/layer/Tile';
+import KML from 'ol/format/KML';
+import {Tile as TileLayer, Vector as VectorLayer} from 'ol/layer';
+import VectorSource from 'ol/source/Vector';
+import {Circle as CircleStyle, Fill, Stroke, Style} from 'ol/style';
 import GeoJSON from 'ol/format/GeoJSON';
+import Circle from 'ol/geom/Circle';
 import Feature from 'ol/Feature';
 import OSM from 'ol/source/OSM';
+import {fromLonLat} from 'ol/proj';
 
-// roadsData = {};
+
+const image = new CircleStyle({
+  radius: 5,
+  fill: null,
+  stroke: new Stroke({color: 'red', width: 1}),
+});
+
+const styles = {
+  'Point': new Style({
+    image: image,
+  }),
+  'LineString': new Style({
+    stroke: new Stroke({
+      color: 'green',
+      width: 1,
+    }),
+  }),
+  'MultiLineString': new Style({
+    stroke: new Stroke({
+      color: 'green',
+      width: 1,
+    }),
+  }),
+  'MultiPoint': new Style({
+    image: image,
+  }),
+  'MultiPolygon': new Style({
+    stroke: new Stroke({
+      color: 'yellow',
+      width: 1,
+    }),
+    fill: new Fill({
+      color: 'rgba(255, 255, 0, 0.1)',
+    }),
+  }),
+  'Polygon': new Style({
+    stroke: new Stroke({
+      color: 'blue',
+      lineDash: [4],
+      width: 3,
+    }),
+    fill: new Fill({
+      color: 'rgba(0, 0, 255, 0.1)',
+    }),
+  }),
+  'GeometryCollection': new Style({
+    stroke: new Stroke({
+      color: 'magenta',
+      width: 2,
+    }),
+    fill: new Fill({
+      color: 'magenta',
+    }),
+    image: new CircleStyle({
+      radius: 10,
+      fill: null,
+      stroke: new Stroke({
+        color: 'magenta',
+      }),
+    }),
+  }),
+  'Circle': new Style({
+    stroke: new Stroke({
+      color: 'red',
+      width: 2,
+    }),
+    fill: new Fill({
+      color: 'rgba(255,0,0,0.2)',
+    }),
+  }),
+};
+
+const styleFunction = function (feature) {
+  return styles[feature.getGeometry().getType()];
+};
 
 function parseData(data) {
   // Start off by drawing truck routes
-}
 
-function setup() {
-  // M.AutoInit();
+  // Convert the projection in the GeoJSON file
+  var n = 0;
+  for (var i = 0; i < data.features.length; i++) {
+    for (var j = 0; j < data.features[i].geometry.coordinates.length; j++) {
+      data.features[i].geometry.coordinates[j] = fromLonLat(data.features[i].geometry.coordinates[j]);
+      n++;
+    }
+  }
+  console.log(n);
+
+  const vectorSource = new VectorSource({
+    features: new GeoJSON().readFeatures(data),
+  });
+  
+  vectorSource.addFeature(new Feature(new Circle([16305945.73750275, -2206013.7191165173], 1e3)));
+
+  const roadsGeoJSONLayer = new VectorLayer({
+    source: vectorSource,
+    style: styleFunction,
+  });  
 
   const map = new Map({
     target: 'map',
     layers: [
       new TileLayer({
         source: new OSM()
-      })
+      }),
+      roadsGeoJSONLayer
     ],
     view: new View({
       center: [16305945.73750275, -2206013.7191165173],
       zoom: 6.1,
-      minZoom: 6, 
-      extent: [15523987.351939877, -3234740.7746837423, 17196894.49780245, -1255714.7470971544],
+      // minZoom: 6, 
+      // extent: [15523987.351939877, -3234740.7746837423, 17196894.49780245, -1255714.7470971544],
       constrainOnlyCenter: true
     })
+  });
+}
+
+function setup() {
+  M.AutoInit();
+
+  const roadsData = new VectorLayer({
+    source: new VectorSource({
+      url: 'data/doc.kml',
+      format: new KML(),
+    }),
   });
 
   // Fetch the GeoJSON data from the server
@@ -39,6 +147,9 @@ function setup() {
       // shaaaaaaaaame shaaaaaaaaaaaaaaaaaaaaaame
     }
   }
+  xhttp.open("GET", "/data/roads.geojson");
+  xhttp.send();
+  
 }
 
 setup();
